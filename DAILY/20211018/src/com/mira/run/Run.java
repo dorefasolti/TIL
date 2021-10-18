31f7ec4541354866eecc57f8d26de074173d5c74
dorefasolti <91889431+dorefasolti@users.noreply.github.com>
2021-10-18 21:15:33 +0900

    Add files via upload

---
+++ DAILY/20211018/src/com/mira/run/Run.java
@@ -0,0 +1,11 @@
+package com.mira.run;
+
+import com.mira.study.ReviewTriple;
+
+public class Run {
+	public static void main(String[] args) {
+		ReviewTriple rt = new ReviewTriple();
+		rt.triple();
+	}
+
+}
